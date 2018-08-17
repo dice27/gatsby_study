fe1f4e7f4ec8cefa1cc4f26e2290cc8f44a4e8be
Yudai Takada <t.yudai92@gmail.com>
2018-08-17 19:36:02 +0900

    Delete unuse style.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -69,7 +69,6 @@ class Template extends React.Component {
             marginLeft: 'auto',
             marginRight: 'auto',
             maxWidth: rhythm(24),
-            //padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
         {children()}
